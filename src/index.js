f02452fff8ba0ae2ef4a153865951fae5680c2ee
Marcio Rodrigues <luiz.rodrigues@eduzz.com>
2020-10-26 21:04:50 -0300

    finalizado entendimento de backend, falta middleware

--- src/index.js
+++ src/index.js
@@ -1,10 +1,69 @@
 const express = require('express');
+const { uuid } = require('uuidv4');
 
 const app = express();
+app.use(express.json());
 
-app.get('/', (request, response) => {
-  return response.json({message: 'Hello de novo '})
+
+const projects = []
+
+
+app.get('/projects', (request, response) => {
+  const {title} = request.query;
+
+  const results = title ? projects.filter(project => project.title.includes(title)) : projects 
+
+  return response.json(results)
 });
 
+app.post('/projects', (request, response) => {
+  const {title, owner} = request.body;
 
-app.listen(3333);
+  const newProject = {id: uuid(), title, owner}
+
+  projects.push(newProject)
+
+  return response.json(newProject)
+});
+
+app.put('/projects/:id', (request, response) => {
+  const {id} = request.params;
+  const {title, owner} = request.body;
+
+  const projectIndex = projects.findIndex(project => project.id === id)
+
+  if (projectIndex < 0) {
+    return response.status(400).json({error: 'Project not found'})
+  }
+
+  const updateProject = {
+    id,
+    title,
+    owner
+  }
+
+  projects[projectIndex] = updateProject;
+
+  return response.json(updateProject)
+})
+
+app.delete('/projects/:id', (request, response) => {
+  const {id} = request.params;
+
+  const projectIndex = projects.findIndex(project => project.id === id)
+
+  if (projectIndex < 0) {
+    return response.status(400).json({error: 'Project not found'})
+  }
+
+  projects.splice(projectIndex, 1);
+
+  return response.status(204).send()
+})
+
+
+app.listen(3333, () => {
+  console.log('************')
+  console.log('Back-end started')
+  console.log('************')
+});
