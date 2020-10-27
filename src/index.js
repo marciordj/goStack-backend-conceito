427a1b5008010c7c32e525fe65697b2985fef2e1
Marcio Rodrigues <luiz.rodrigues@eduzz.com>
2020-10-27 09:32:43 -0300

    add middleware

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { uuid } = require('uuidv4');
+const { uuid, isUuid } = require('uuidv4');
 
 const app = express();
 app.use(express.json());
@@ -7,6 +7,28 @@ app.use(express.json());
 
 const projects = []
 
+function logRequest(request, response, next) {
+  const { method, url } = request;
+
+  const logLabel = `[${method.toUpperCase()}] ${url}`
+
+  console.log(logLabel)
+
+  return next()
+}
+
+function validateProjectId(request, response, next) {
+  const { id } = request.params;
+
+  if (!isUuid(id)) {
+    return response.status(400).json({error: 'Invalid project ID.'});
+  }
+
+  return next()
+}
+
+app.use(logRequest);
+app.use('/projects/:id', validateProjectId);
 
 app.get('/projects', (request, response) => {
   const {title} = request.query;
